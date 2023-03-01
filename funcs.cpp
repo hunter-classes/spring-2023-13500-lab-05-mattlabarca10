d6f6918fb6b0f09f2fcc917a73bf37682cca3a43
matt <matlab0510@gmail.com>
2023-03-01 03:10:20 -0500

    lab 5 work

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,73 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <iostream>
-#include "funcs.h"
 
-// add functions here
 
+bool isDivisibleBy(int n, int d)
+{
+    if(n%d == 0)
+        return true;
+    else 
+        return false;
+}
+
+bool isPrime(int n)
+{
+    if(n>=2){
+        for(int i = 2; i < (n/2+1); i++){
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    else
+        return false;
+}
+
+int nextPrime(int n)
+{
+    int i = n+1;
+    while(isPrime(i)==false)
+        i++;
+    return i;
+}
+
+int countPrimes(int a, int b)
+{
+    int count = 0;
+    for(int i = a; i <= b; i++){
+        if(isPrime(i)==true)
+            count++;
+    }
+    return count;
+}
+
+
+bool isTwinPrime(int n)
+{
+    if(isPrime(n)==true){
+        if(isPrime(n+2) || isPrime(n-2))
+            return true;
+        else    
+            return false;
+    }
+    else
+        return false;
+}
+
+int nextTwinPrime(int n)
+{
+    int i = n+1;
+    while(isTwinPrime(i)==false)
+        i++;
+    return i;
+}
+
+int largestTwinPrime(int a, int b)
+{
+    int largest = -1;
+    for(int i = a; i <= b; i++){
+        if(isTwinPrime(i)==true)
+            largest = i;
+    }
+    return largest;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,22 @@
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
+  std::cout << isDivisibleBy(100,25) << std::endl;
+  std::cout << isDivisibleBy(35,17) << std::endl;
+  std::cout << isPrime(3) << std::endl;
+  std::cout << isPrime(16) << std::endl;
+  std::cout << isPrime(17) << std::endl;
+  std::cout << nextPrime(3) << std::endl;
+  std::cout << nextPrime(17) << std::endl;
+  std::cout << countPrimes(3,17) << std::endl;
+  std::cout << countPrimes(17,50) << std::endl;
+  std::cout << isTwinPrime(3) << std::endl;
+  std::cout << isTwinPrime(67) << std::endl;
+  std::cout << nextTwinPrime(3) << std::endl;
+  std::cout << nextTwinPrime(17) << std::endl;
+  std::cout << largestTwinPrime(3,17) << std::endl;
+  std::cout << largestTwinPrime(17,50) << std::endl;
   return 0;
 }
